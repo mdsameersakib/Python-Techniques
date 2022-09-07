bf567121cd0e50d4b7f29e2ba0888f3b3e7bb90a
Md. Sameer Sakib <mdsameersakib@gmail.com>
2022-09-07 08:16:09 +0600

    Update List Functions ( Manual ).py

--- List Functions ( Manual ).py
+++ List Functions ( Manual ).py
@@ -12,17 +12,24 @@ if __name__ == '__main__':
     l = []
     for i in range(N):
         user = input("").split(" ")
-        if user[0]=="insert":
-            l.insert(int(user[1]),int(user[2]))   
-        elif user[0]=="remove":
+        
+        if user[0]=="insert":#━━━━━━━━━━━━━━━━━━ .insert(index,value) -- inserts item into index
+            l.insert(int(user[1]),int(user[2])) 
+            
+        elif user[0]=="remove":#━━━━━━━━━━━━━━━━ .remove(value) -- removes first found item 
             l.remove(int(user[1]))
-        elif user[0]=="sort":
+            
+        elif user[0]=="sort":#━━━━━━━━━━━━━━━━━━ .sort() -- sorts a list
             l.sort()
-        elif user[0]=="append":
-            l.append(int(user[1]))            
-        elif user[0]=="pop":
+            
+        elif user[0]=="append":#━━━━━━━━━━━━━━━━ .append(value) -- adds a item to end of list
+            l.append(int(user[1]))
+            
+        elif user[0]=="pop":#━━━━━━━━━━━━━━━━━━━ .pop(index) -- removes item from index
             l.pop()
-        elif user[0]=="print":
-            print(l)
-        elif user[0]=="reverse":
+            
+        elif user[0]=="reverse":#━━━━━━━━━━━━━━━ .reverse() -- reverses the list
             l.reverse()
+            
+        elif user[0]=="print": 
+                    print(l)
