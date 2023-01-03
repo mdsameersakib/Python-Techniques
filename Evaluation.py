53b032c0317ce7a84c1f401470370838ab427e71
Md. Sameer Sakib <mdsameersakib@gmail.com>
2023-01-03 22:40:53 +0600

    Update Evaluation.py

--- Evaluation.py
+++ Evaluation.py
@@ -4,3 +4,7 @@ print(eval("x+1+x**2+x**3"))
 # gives the output as:
 156 
 # NB whatever we put inside eval should be a string otherwise it won't work...
+# Something more interesting 
+if user input is "print(2+3)"
+we can just do 
+eval(input())
